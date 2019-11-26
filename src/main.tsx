207bf1717c086b50c4a30c88c78f9d05cc4cb3a6
Matt Gadd <mgadd@sitepen.com>
2019-11-26 15:06:30 +0000

    sandpit setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,8 @@
-import './examples/src/main';
+import main from '@dojo/example-runner/main';
+import configs from './examples/src/config';
+
+import dojo from '@dojo/themes/dojo';
+import '@dojo/themes/dojo/index.css';
+
+const tests = (require as any).context('./', true, /\.spec\.ts(x)?$/);
+main({ domNode: document.getElementById('app')!, configs, themes: [dojo], tests });
